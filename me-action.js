1be6eceb776fd421d393c358a1b8692df3e375b4
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-04-07 18:13:55 -0400

    Update me-action.js

--- me-action.js
+++ me-action.js
@@ -1,7 +1,7 @@
 var me = new iChatPlugin("action/me", function(data) {
     if (data.txt.startsWith("/me ")) {
-      data.txt = "*"+data.u+" "+data.txt.substring(4);
-      data.u = "#"
+      data.txt = "***"+data.u+" "+data.txt.substring(4);
+      data.u = null;
     }
     return data;
 }, "Written by jcgter777, using code from iPhoenix.");
