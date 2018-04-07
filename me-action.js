7aed14f8a91e94cd3e918f3c4b1e47806ca81d59
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-04-07 17:21:26 -0400

    Create me-action.js

---
+++ me-action.js
@@ -0,0 +1,11 @@
+var me = new iChatPlugin("action/me", function(data) {
+    if (data.txt.startsWith("/me ")) {
+      data.txt = "*"+data.u+" "+data.txt.substring(4);
+      data.u = "#"
+    }
+    return data;
+}, "Written by jcgter777, using code from iPhoenix.");
+
+iChat.onload = function() {
+    iChat.registerPlugin(me);
+}
